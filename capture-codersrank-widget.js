dd1355f69be9b2253d2807e2f581b5b228c468af
Sutherland Nele <51810575+sutherlandnele@users.noreply.github.com>
2024-05-11 11:45:29 +1000

    Update capture-codersrank-widget.js

--- capture-codersrank-widget.js
+++ capture-codersrank-widget.js
@@ -1,9 +1,10 @@
 const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   const filePath = `file://${process.env.WORKSPACE_PATH}/codersrank-widget.html`;
+  console.log(filePath);
   await page.goto(filePath, {waitUntil: 'networkidle0'});
   await page.screenshot({path: 'codersrank-widget.png'});
   await browser.close();
